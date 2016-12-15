08a4b28c8b85794c8862701f07b06f63d7a71977
Jonas Wälter <waelter@outlook.de>
2016-12-15 08:17:03 +0100

    AddEditGadget MVVM

--- ch.hsr.wpf.gadgeothek.app/AddEditGadget.xaml.cs
+++ ch.hsr.wpf.gadgeothek.app/AddEditGadget.xaml.cs
@@ -21,52 +21,13 @@ namespace ch.hsr.wpf.gadgeothek.app
     /// </summary>
     public partial class AddEditGadget : MetroWindow
     {
-        public Gadget OriginalGadget { get; set; }
-        public Gadget Gadget { get; set; }
-        private bool IsNew { get; set; }
-        private int MaxID { get; set; }
+        public AddEditViewModel AddEditViewModel { get; set; }
 
         public AddEditGadget(Gadget gadget)
         {
             InitializeComponent();
-            OriginalGadget = gadget;
-            Gadget = (Gadget) OriginalGadget.Clone();
-            IsNew = (Gadget.InventoryNumber == null);
-            DataContext = this;
+            AddEditViewModel = new AddEditViewModel(gadget);
+            DataContext = AddEditViewModel;
         }
-
-        private void bSaveGadget_Click(object sender, RoutedEventArgs e)
-        {
-            if (IsNew)
-            {
-                Gadget.InventoryNumber = GetNewGadgetID();
-                if (MainWindow.Service.AddGadget(Gadget))
-                {
-                    GadgetViewModel.Gadgets.Add(Gadget);
-                } else
-                {
-                    MessageBoxResult messageBoxResult = MessageBox.Show("Could not add this gadget.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-            } else
-            {
-                if(MainWindow.Service.UpdateGadget(Gadget))
-                {
-                    OriginalGadget.Name = Gadget.Name;
-                    OriginalGadget.Manufacturer = Gadget.Manufacturer;
-                    OriginalGadget.Price = Gadget.Price;
-                    OriginalGadget.Condition = Gadget.Condition;
-                } else
-                {
-                    MessageBoxResult messageBoxResult = MessageBox.Show("Could not update selected gadget.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-            }
-            DialogResult = true;
-        }
-
-        private string GetNewGadgetID()
-        {
-            return (GadgetViewModel.Gadgets.Max(x => x.InventoryNumber != null ? Int32.Parse(x.InventoryNumber) : 0) + 1).ToString();
-        }
-
     }
 }
---
+++ ch.hsr.wpf.gadgeothek.app/AddEditViewModel.cs
@@ -0,0 +1,70 @@
+﻿using ch.hsr.wpf.gadgeothek.domain;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using ch.hsr.wpf.gadgeothek.service;
+using System.Linq;
+
+namespace ch.hsr.wpf.gadgeothek.app
+{
+    public class AddEditViewModel
+    {
+        public ICommand SaveCommand { get; set; } = new RelayCommand<Window>((x) => Save(x), (x) => CanSave(Gadget));
+
+        public static LibraryAdminService Service = new LibraryAdminService("http://mge5.dev.ifs.hsr.ch/");
+        public static Gadget OriginalGadget { get; set; }
+        public static Gadget Gadget { get; set; }
+        private static bool IsNew { get; set; }
+        private static int MaxID { get; set; }
+
+        public AddEditViewModel(Gadget gadget)
+        {
+            OriginalGadget = gadget;
+            Gadget = (Gadget)OriginalGadget.Clone();
+            IsNew = (Gadget.InventoryNumber == null);
+        }
+
+
+        public static bool CanSave(Gadget gadget) => 
+            gadget.Manufacturer != String.Empty &&
+            gadget.Name != String.Empty &&
+            gadget.Price >= 0;
+        public static void Save(Window win)
+        {
+            if (IsNew)
+            {
+                Gadget.InventoryNumber = GetNewGadgetID();
+                if (MainViewModel.Service.AddGadget(Gadget))
+                {
+                    MainViewModel.Gadgets.Add(Gadget);
+                }
+                else
+                {
+                    MessageBoxResult messageBoxResult = MessageBox.Show("Could not add this gadget.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                if (MainViewModel.Service.UpdateGadget(Gadget))
+                {
+                    OriginalGadget.Name = Gadget.Name;
+                    OriginalGadget.Manufacturer = Gadget.Manufacturer;
+                    OriginalGadget.Price = Gadget.Price;
+                    OriginalGadget.Condition = Gadget.Condition;
+                }
+                else
+                {
+                    MessageBoxResult messageBoxResult = MessageBox.Show("Could not update selected gadget.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            win.Close();
+        }
+
+        private static string GetNewGadgetID()
+        {
+            return (Service.GetAllGadgets().Max(x => x.InventoryNumber != null ? Int32.Parse(x.InventoryNumber) : 0) + 1).ToString();
+        }
+    }
+}
